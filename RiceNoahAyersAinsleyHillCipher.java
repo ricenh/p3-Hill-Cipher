2858308d0d7d38803472d324eabd39789c0c91e0
ricneh <ricenh@dukes.jmu.edu>
2024-10-23 17:46:23 -0400

    completed encrypt

--- RiceNoahAyersAinsleyHillCipher.java
+++ RiceNoahAyersAinsleyHillCipher.java
@@ -10,19 +10,30 @@ public class RiceNoahAyersAinsleyHillCipher {
         decryptionKey[0][1] = gcd * -1 * encryptionKey[0][1] % 26;
         decryptionKey[1][0] = gcd * -1 * encryptionKey[1][0] % 26;
         decryptionKey[1][1] = gcd * encryptionKey[0][0] % 26;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (decryptionKey[i][j] < 0) {
+                    decryptionKey[i][j] += 26;
+                }
+            }
+        }
         return decryptionKey;
     }
 
     public static int[] encrypt(int plaintext[], int encryptionKey[][]) {
-        return new int[2];
+        int[] encrypt= new int[2];
+        encrypt[0] = (encryptionKey[0][0] * plaintext[0] + encryptionKey[0][1] * plaintext[1]) % 26;
+        encrypt[1] = (encryptionKey[1][0] * plaintext[0] + encryptionKey[1][1] * plaintext[1]) % 26;
+        return encrypt;
     }
 
     public static int[] decrypt(int ciphertext[], int decryptionKey[][]) {
         return new int[2];
     }
 
-    public static String intToString (int num) {
-        return "";
+    public static String intToString(int num) {
+        char character = (char) (num + 'A'); 
+        return String.valueOf(character); 
     }
 
     public static int stringToInt(String cipher) {
@@ -68,16 +79,38 @@ public class RiceNoahAyersAinsleyHillCipher {
         encryptionKey[0][1] = 7;
         encryptionKey[1][0] = 9;
         encryptionKey[1][1] = 14;
+        System.out.println("Encyption Key:");
+        System.out.printf("%s %s\n", encryptionKey[0][0], encryptionKey[0][1]);
+        System.out.printf("%s %s\n", encryptionKey[1][0], encryptionKey[1][1]);
+        
         int[][] decryptionKey = findDecryptionKey(encryptionKey);
-        System.out.println(encryptionKey[0][0]);
-        System.out.println(encryptionKey[0][1]);
-        System.out.println(encryptionKey[1][0]);
-        System.out.println(encryptionKey[1][1]);
+        System.out.println("Decyption Key:");
+        System.out.printf("%s %s\n", decryptionKey[0][0], decryptionKey[0][1]);
+        System.out.printf("%s %s\n", decryptionKey[1][0], decryptionKey[1][1]);
 
-        System.out.println(decryptionKey[0][0]);
-        System.out.println(decryptionKey[0][1]);
-        System.out.println(decryptionKey[1][0]);
-        System.out.println(decryptionKey[1][1]);
-
+        int[][] plaintexts = {
+            {9, 12},
+            {20, 2},
+            {18, 8},
+            {18, 2},
+            {14, 14},
+            {11, 25}
+        };
+        
+        String[] encryptedStrings = new String[plaintexts.length * 2];
+        for (int i = 0; i < plaintexts.length; i++) {
+            int[] ciphertext = encrypt(plaintexts[i], encryptionKey);
+            encryptedStrings[i * 2] = intToString(ciphertext[0]);
+            encryptedStrings[i * 2 + 1] = intToString(ciphertext[1]);
+        }
+        
+        System.out.println("Encryption of JMUCSISCOOL:");
+        System.out.printf("%s%s %s%s %s%s %s%s %s%s %s%s\n",
+            encryptedStrings[0], encryptedStrings[1],
+            encryptedStrings[2], encryptedStrings[3],
+            encryptedStrings[4], encryptedStrings[5],
+            encryptedStrings[6], encryptedStrings[7],
+            encryptedStrings[8], encryptedStrings[9],
+            encryptedStrings[10], encryptedStrings[11]);
     }
 }
